9d98b5a3d81c3cb032fe72f03605c6ea97627060
John Fuquene <jfuquene@email.wm.edu>
2020-11-11 21:17:58 +0000

    Automatically backed up by Learn

--- lib/math.rb
+++ lib/math.rb
@@ -5,21 +5,31 @@ def addition(num1, num2)
 end
 
 def subtraction(num1, num2)
+  subtract = (num2 - num1)
+  return subtract
 
 end
 
 def division(num1, num2)
+  divide = (num2 / num1)
+  return divide 
 
 end
 
 def multiplication(num1, num2)
+  multiply (num2 * num1)
+  return multiply
 
 end
 
 def modulo(num1, num2)
+  modul = (num2 % num1)
+  return modul
 
 end
 
 def square_root(num)
+  square (num**)
+  return square
 
 end
